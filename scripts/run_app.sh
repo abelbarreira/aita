ef848619b6f9c12df0b180e24a4902ac630c991d
abelbarreira <abelbarreira@hotmail.com>
2025-08-08 08:43:35 +0200

    chore: commented prompt usage from run app script

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -14,7 +14,7 @@ echo "🚀 Running aita..."
 hatch run aita -- --version
 echo
 
-hatch run aita -- --prompt "From Copenhagen to Rome staying between 5 to 10 days in July, looking for a 4 stars hotel in area Colosseum. All-Inclusive preferred."
-echo
+# hatch run aita -- --prompt "From Copenhagen to Rome staying between 5 to 10 days in July, looking for a 4 stars hotel in area Colosseum. All-Inclusive preferred."
+# echo
 
 popd > /dev/null
