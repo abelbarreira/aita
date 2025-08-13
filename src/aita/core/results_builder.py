b4603200eb2d7e2e190794782737033f1166ecc3
abelbarreira <abelbarreira@hotmail.com>
2025-08-13 18:38:33 +0200

    feat: added results builder with ResultsAirports class

---
+++ src/aita/core/results_builder.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+from dataclasses import dataclass
+
+
+@dataclass
+class ResultsAirports:
+    """
+    Represents ...
+    """
+
+    id: str
+    airport_name: str
+
+
+def pretty_print_results_airports(results_airports: dict[int, ResultsAirports]) -> None:
+    """
+    Nicely prints all entries in a dict[int, ResultsAirports].
+    """
+    for key, airport in results_airports.items():
+        print(f"ID {key}: id = {airport.id}, airport_name = {airport.airport_name}")
